2d7fb501692ed77c11b798267fa674317770e129
Samuel Santos <samuel.cezar@ykt.com.br>
2021-11-22 12:59:03 -0300

    upload

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'> 
+        <strong> 
+            Copyright &copy; 2021
+            <a href='http://google.com.br' target='_blank'> Samu</a>.
+        </strong>
+    </footer>
+)
